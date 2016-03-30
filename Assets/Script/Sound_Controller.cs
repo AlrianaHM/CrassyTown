ead3cb9f3c324f22dcb7206dd60427ffb8780465
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2016-03-30 14:43:28 +0700

    sound

---
+++ Assets/Script/Sound_Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound_Controller : MonoBehaviour {
+
+	public GameObject aud;
+	public volume vol;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 scale = new Vector3 (0, 0, 0);
+		if (GameObject.FindGameObjectWithTag ("Audio")) {
+			aud = GameObject.FindGameObjectWithTag ("Audio");
+		}
+		vol = aud.GetComponent<volume> ();
+		if(vol.loudness > 5f)
+			transform.localScale = new Vector3 (vol.loudness, 0.6f, 0.6f);
+		else transform.localScale = new Vector3 (0f, 0.6f, 0.6f);
+			
+			
+	}
+}
---
+++ Assets/Script/volume.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class volume : MonoBehaviour {
+
+	public float sensitivity = 0.1f;
+	public float loudness = 0;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource>().clip = Microphone.Start (null, true, 10, 44100);
+		GetComponent<AudioSource>().loop = true;
+		//GetComponent<AudioSource>().mute = true;
+		while (!(Microphone.GetPosition ("AudioInputDevice") > 0 )) {
+		}
+		GetComponent<AudioSource>().Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loudness = GetAverageVol () * sensitivity;
+		Debug.Log ("Loudness: " + loudness);
+	}
+
+	float GetAverageVol(){
+		float[] data = new float[1024];
+		float a = 0;
+		GetComponent<AudioSource>().GetOutputData (data, 0);
+		foreach (float s in data) {
+			a += Mathf.Abs (s);
+		}
+
+		return a / 1024;
+	}
+
+}
