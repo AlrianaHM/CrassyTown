4adbc05fdfb7d350dc1d62827b41bff2f30e91bd
David <davidkwan95@gmail.com>
2016-04-26 23:26:06 +0700

    Add some basic gameplay
    Zoom in, spawn enemy, and destroying enemy.
    Not done: showing enemy loudness parameter, add score, game over

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private GameObject screamListener;
+	private float loudness;
+	public float winLoudness = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		screamListener = GameObject.Find ("Scream_Listener");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loudness = screamListener.GetComponent<volume> ().loudness;
+		if (loudness > winLoudness) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Script/Game_Controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Controller : MonoBehaviour {
+
+	public Transform background;
+	public float zoomSpeed = 0.01f;
+	private bool moving = false;
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		moving = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving) {
+			float scaleX = background.localScale.x;
+			float scaleY = background.localScale.y;
+			float scaleZ = background.localScale.z;
+
+			background.localScale = new Vector3 (scaleX + zoomSpeed, scaleY + zoomSpeed, scaleZ + zoomSpeed);
+
+			if (scaleX > 1.3f) {
+				moving = false;
+				Instantiate (enemy);
+			}
+		}
+
+	}
+}
